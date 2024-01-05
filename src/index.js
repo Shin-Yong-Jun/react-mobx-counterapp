781fe94f79e22b161e74d6d0495a8d0f3ca86da4
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-05 22:49:03 +0900

    카운터 UI 구현 하지만 구독subscibe 및 observe가 안되서 실제 반영이 안됨. 모듈설치예정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import counterStore from './countStore';
+
+
+// 여기서 store 인스턴스 생성
+const store = new counterStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App myCounter={store}/>
   </React.StrictMode>
 );
 
